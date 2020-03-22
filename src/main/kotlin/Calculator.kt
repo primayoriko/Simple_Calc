35506bf979379adb0f7ddf3d3c2b4d75738cc485
primayoriko <primayoriko@gmail.com>
2020-03-23 01:31:49 +0700

    update 18 - semoga cepet kelar

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -8,17 +8,17 @@ import tornadofx.*
 class Calculator : View(){
     override val root: VBox by fxml()
     @FXML lateinit var display: Label
-    var operation = BasicOperator()
+    var command = BasicCommand()
     init {
         title = "Simple Calc"
         root.lookupAll(".button").forEach { b ->
             b.setOnMouseClicked {
-                display.text = operation.operate((b as Button).text)
+                display.text = command.doTask((b as Button).text)
             }
         }
 
         root.addEventFilter(KeyEvent.KEY_TYPED) {
-            display.text = operation.operate(it.character.toUpperCase().replace("\r", "="))
+            display.text = command.doTask(it.character.toUpperCase().replace("\r", "="))
         }
     }
 }
