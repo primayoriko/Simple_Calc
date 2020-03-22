a25a7a2e547f368620b31e65276103da31524802
primayoriko <primayoriko@gmail.com>
2020-03-23 05:14:15 +0700

    update 27 - menulis test file

---
+++ src/main/kotlin/TestEquation.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>){
+    var test = Equation()
+
+    test.addToken("2")
+    test.addToken("4")
+    test.addToken("+")
+    test.addToken("3")
+    test.addToken()
+    println(test.printEquation()) // print 24+3
+    println(test.calculate()) // print 27
+    test.removeToken()
+    println(test.printEquation()) //print 24+
+    test.clear()
+    println(test.printEquation()) //print 'kosong/null' / hanya print newline
+    if(test.isCurEmpty()){
+        println("a")           // print a karena kosong
+    }
+    if(test.isValidNum("1234")){
+        println("a")           // print a karena 1234 string penyusun angka valid
+    }
+}
