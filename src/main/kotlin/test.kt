b90a1667db2060ea4942bc52afd58443dcba0e40
primayoriko <primayoriko@gmail.com>
2020-03-23 03:18:52 +0700

    update 24 - fixing regex

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -10,6 +10,32 @@ fun main(args: Array<String>) {
 //    println(a.solve());
 //    val np = AddExpression(TerminalExpression(5.0), TerminalExpression(7.0));
 //    println(np.solve());
-    var s : String = "sadasd"
-    println(s.slice(0..2))
+    var test = Equation()
+    test.addToken("<-")
+    println(test.elmt.size)
+    test.addToken("2")
+    println(test.elmt.size)
+    test.addToken("2")
+    println(test.elmt.size)
+    test.addToken("+")
+    println(test.elmt.size)
+    test.addToken("2")
+    println(test.elmt.size)
+    test.addToken("2")
+    println(test.elmt.size)
+    test.addToken(".")
+    println(test.elmt.size)
+    test.addToken("0")
+    println(test.elmt.size)
+    test.addToken("2")
+    println(test.elmt.size)
+    test.addToken("x")
+    println(test.elmt.size)
+    test.addToken("x")
+    println(test.elmt.size)
+    test.addToken("<-")
+    println(test.elmt.size)
+
+    println(test.printEquation())
+    //println(test.calculate())
 }
