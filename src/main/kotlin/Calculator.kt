13a599e8e9f549c7a1354a6faa291ce3923fbb95
primayoriko <primayoriko@gmail.com>
2020-03-23 01:11:07 +0700

    update 16 - basicoperator class

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -4,54 +4,21 @@ import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.VBox
 import tornadofx.*
-import Expression.*
 
 class Calculator : View(){
-    var ans = TerminalExpression()
-    var queue : MutableList<Double> = arrayListOf();
-    var equation = Equation()
-    
     override val root: VBox by fxml()
     @FXML lateinit var display: Label
+    var operation = BasicOperator()
     init {
         title = "Simple Calc"
         root.lookupAll(".button").forEach { b ->
             b.setOnMouseClicked {
-                operator((b as Button).text)
+                display.text = operation.operate((b as Button).text)
             }
         }
 
         root.addEventFilter(KeyEvent.KEY_TYPED) {
-            operator(it.character.toUpperCase().replace("\r", "="))
+            display.text = operation.operate(it.character.toUpperCase().replace("\r", "="))
         }
     }
-
-    private fun operator(x: String) {
-        if(x.equals("=")){
-            display.text = equation.calculate()
-            ans.setValue(display.text.toDouble())
-        }
-        else if(x.equals("AC")){
-            display.text = equation.calculate()
-            ans.setValue(display.text.toDouble())
-        }
-        else if(x.equals("MR")){
-            display.text = equation.calculate()
-            ans.setValue(display.text.toDouble())
-        }
-        else if(x.equals("MC")){
-            display.text = equation.calculate()
-            ans.setValue(display.text.toDouble())
-        }
-        else if(x.equals("Ans")){
-            display.text = equation.calculate()
-            ans.setValue(display.text.toDouble())
-        }
-        else{
-            equation.addToken(x)
-            display.text = equation.printEquation()
-        }
-
-    }
-
 }
