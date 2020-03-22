1698bb5dcd6ffa673ddd68753ebae30a78e69acd
Lionnarta <lionnarta.savirandy2000@gmail.com>
2020-03-23 02:18:50 +0700

    calculate

--- src/main/kotlin/Equation.kt
+++ src/main/kotlin/Equation.kt
@@ -94,21 +94,74 @@ class Equation : EquationGeneric<String>{
     }
 
     override public fun calculate(): String{
-        var result = ""
-        this.elmt.forEach { i->{
-            if(i.first.equals("operand")){
-                if(isValidNum(i.second)){
+        var stack : MutableList<Pair<String,String>> = mutableListOf<Pair<String,String>>();
+        var result : String;
+        var numb : Double;
+        var numb2 : Double;
+        var op : String;
+        this.elmt.forEach { i->stack.add(Pair<String, String>(i.first, i.second))}
 
+        while(stack.size == 1){
+            if(isValidNum(stack[stack.lastIndex].second)){
+                numb = stack[stack.lastIndex].second.toDouble();
+                stack.removeAt(stack.lastIndex);
+                if(stack.isNotEmpty()){
+                    op = stack[stack.lastIndex].second;
+                    stack.removeAt(stack.lastIndex);
+                    if(op.equals("sqrt") || op.equals("sin") || op.equals("cos") || op.equals("tan") || (op.equals("-") && stack.isNotEmpty() && stack[stack.lastIndex].first.equals("operator"))){
+                        result = when(op)
+                        {
+                            "-" -> {
+                                NegativeExpression(TerminalExpression(numb)).solve().toString()
+                            }
+                            "sqrt" -> RootSquareExpression(TerminalExpression(numb)).solve().toString()
+                            "sin" -> TrigonometricExpression(TerminalExpression(numb)).solve(1).toString()
+                            "cos" -> TrigonometricExpression(TerminalExpression(numb)).solve(2).toString()
+                            "tan" -> TrigonometricExpression(TerminalExpression(numb)).solve(3).toString()
+                            else -> throw ArithmeticException("E")
+                        }
+                        if(op.equals("-") && stack.isNotEmpty() && stack[stack.lastIndex].first.equals("operator")){
+                            stack.removeAt(stack.lastIndex);
+                        }
+                        stack.add(Pair<String, String>("operand", result));
+                    }else
+                    {
+                        if(stack.isNotEmpty() && isValidNum(stack[stack.lastIndex].second))
+                        {
+                            numb2 = stack[stack.lastIndex].second.toDouble();
+                            stack.removeAt(stack.lastIndex);
+                            if(stack.isNotEmpty() && stack[stack.lastIndex].second.equals("-") && stack[stack.lastIndex-1].first.equals("operator"))
+                            {
+                                stack.removeAt(stack.lastIndex);
+                                numb2 = NegativeExpression(TerminalExpression(numb2)).solve();
+                            }
+                            
+                            result = when(op)
+                            {
+                                "+" -> AddExpression(TerminalExpression(numb), TerminalExpression(numb2)).solve().toString()
+                                "-" -> SubstractExpression(TerminalExpression(numb), TerminalExpression(numb2)).solve().toString()
+                                "*" -> MultipleExpression(TerminalExpression(numb), TerminalExpression(numb2)).solve().toString()
+                                "/" -> DivideExpression(TerminalExpression(numb), TerminalExpression(numb2)).solve().toString()
+                                "^" -> PowerExpression(TerminalExpression(numb), TerminalExpression(numb2)).solve().toString()
+                                else -> throw ArithmeticException("E");
+                            }
+                            stack.add(Pair<String,String>("operand",result));
+                        }else
+                        {
+                            throw ArithmeticException("E");
+                        }
+                    }
+                }else
+                {
+                    result = numb.toString();
+                    stack.add(Pair<String,String>("operand",result));
                 }
-                else{
-                    //throw error
-                }
+            }else
+            {
+                throw ArithmeticException("E");
             }
-            else{
-
-            }
-        } }
-
-        return result
+        }
+        result = stack[stack.lastIndex].second;
+        return result;
     }
 }
