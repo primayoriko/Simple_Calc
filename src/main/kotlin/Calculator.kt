2358ba1005f19245c4c633b2dea48f1134af573a
primayoriko <primayoriko@gmail.com>
2020-03-23 00:45:45 +0700

    update 15

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -8,10 +8,6 @@ import Expression.*
 
 class Calculator : View(){
     var ans = TerminalExpression()
-    var value = TerminalExpression()
-    var y1 = TerminalExpression()
-    var y2 = TerminalExpression()
-    var operatorExpression: String ="";
     var queue : MutableList<Double> = arrayListOf();
     var equation = Equation()
     
@@ -30,103 +26,32 @@ class Calculator : View(){
         }
     }
 
-//    var state: Operator = add(0)
-//
-//    fun onAction(fn: Operator) {
-//        state = fn
-//        display.text = ""
-//    }
-
-//    val displayValue: Long
-//        get() = when(display.text) {
-//            "" -> 0
-//            else -> display.text.toLong()
-//        }
-
     private fun operator(x: String) {
         if(x.equals("=")){
             display.text = equation.calculate()
+            ans.setValue(display.text.toDouble())
+        }
+        else if(x.equals("AC")){
+            display.text = equation.calculate()
+            ans.setValue(display.text.toDouble())
+        }
+        else if(x.equals("MR")){
+            display.text = equation.calculate()
+            ans.setValue(display.text.toDouble())
+        }
+        else if(x.equals("MC")){
+            display.text = equation.calculate()
+            ans.setValue(display.text.toDouble())
+        }
+        else if(x.equals("Ans")){
+            display.text = equation.calculate()
+            ans.setValue(display.text.toDouble())
+        }
+        else{
+            equation.addToken(x)
+            display.text = equation.printEquation()
+        }
 
-        }
-        if (Regex("[0-9.]").matches(x)) {
-            display.text += x
-            value.addDigit(x.toDouble());
-        } else {
-            // Binary Operator
-            if(true)
-            {
-                display.text += x
-                y1.addDigit(value.solve());
-                value.reset();
-                operatorExpression = x;
-            }
-            // Unary Operator n Khusus
-            else{
-                if(x.equals("="))
-                {
-                    y2.addDigit(value.solve());
-                    kalkulasi();
-                    display.text = ans.solve().toString();
-                    value.reset();
-                }
-                else if(x.equals("Ans")){
-                    y1.reset();
-                    y1.addDigit(value.solve());
-                }
-                else if(x.equals("MC")){
-                    queue.add(value.solve());
-                }
-                else if(x.equals("MR")){
-                    if(queue.isEmpty())
-                    {
-                        value.reset();
-                    }else
-                    {
-                        value.addDigit(queue.removeAt(0));
-                    }
-                }
-                else if(x.equals("<-")) {
+    }
 
-                }
-                else if (x.equals("-") || x.equals("sqrt") || x.equals("cos") || x.equals("sin") || x.equals("tan"))
-                {
-                    operatorExpression = x;
-                }
-            }
-        }
-    }
-    private fun kalkulasi()
-    {
-        if(operatorExpression.equals("+"))
-        {
-            ans.addDigit(AddExpression(y1, y2).solve());
-        }else if(operatorExpression.equals("-"))
-        {
-            ans.addDigit(SubstractExpression(y1, y2).solve());
-        }else if(operatorExpression.equals("*"))
-        {
-            ans.addDigit(MultipleExpression(y1, y2).solve());
-        }else if(operatorExpression.equals("/"))
-        {
-            ans.addDigit(DivideExpression(y1, y2).solve());
-        }else if(operatorExpression.equals("^"))
-        {
-            ans.addDigit(PowerExpression(y1, y2).solve());
-        }else if(operatorExpression.equals("sqrt"))
-        {
-            ans.addDigit(RootSquareExpression(y1).solve());
-        }else if(operatorExpression.equals("sin"))
-        {
-            ans.addDigit(TrigonometricExpression(y1).solve(1));
-        }else if(operatorExpression.equals("cos"))
-        {
-            ans.addDigit(TrigonometricExpression(y1).solve(2));
-        }else if(operatorExpression.equals("tan"))
-        {
-            ans.addDigit(TrigonometricExpression(y1).solve(3));
-        }else if(operatorExpression.equals("ans"))
-        {
-            
-        }
-    }
 }
